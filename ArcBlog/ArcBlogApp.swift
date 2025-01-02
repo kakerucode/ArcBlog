6de9873ffcdb4cf85fb011f1d7c403133eba3107
Xiang Liu <kakerucode@gmail.com>
2025-01-02 11:21:47 +0800

    Initial Commit

---
+++ ArcBlog/ArcBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcBlogApp.swift
+//  ArcBlog
+//
+//  Created by 刘翔 on 2025/1/2.
+//
+
+import SwiftUI
+
+@main
+struct ArcBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
